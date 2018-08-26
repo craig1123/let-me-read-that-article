e372c6b98bc03cd3a517a46acaba43cc7d33e368
Craig <craigwalker1123@gmail.com>
2018-08-25 20:13:13 -0600

    starting clear cookies

--- src/background.js
+++ src/background.js
@@ -1,1 +1,13 @@
 console.log('background');
+
+chrome.runtime.onMessage.addListener(function(request) {
+    if (request.type == "clearCookies") {
+        chrome.cookies.getAll({ domain: request.domain }, function(cookies) {
+            for (var i = 0; i < cookies.length; i++) {
+                console.log(request.domain + cookies[i].path);
+                chrome.cookies.remove({ url: request.domain + cookies[i].path, name: cookies[i].name});
+            }
+            console.log(cookies);
+        });
+    }
+});
--- src/content.js
+++ src/content.js
@@ -8,20 +8,15 @@ var domains = {
 setTimeout(checkPopup, 3000);
 
 function checkPopup() {
-    var domain = domains[document.location.host];
-    if (domain === 'tp-modal' && document.activeElement === document.body) {
+    var domainMessage = domains[document.location.host];
+    if (domainMessage === 'tp-modal' && document.activeElement === document.body) {
         var tpModal = document.getElementsByClassName('tp-modal')[0];
         if (tpModal) {
             document.getElementsByClassName('tp-modal')[0].classList.remove('tp-modal');
             document.getElementsByClassName('tp-backdrop tp-active')[0].classList.remove('tp-active');
             document.body.classList.remove('tp-modal-open');
         }
-    } else if (domain === clearCookies) {
-        chrome.cookies.getAll({ domain: domain }, function(cookies) {
-            console.log(cookies);
-            // for (var i = 0; i < cookies.length; i++) {
-            //     chrome.cookies.remove({ url: domain + cookies[i].path, name: cookies[i].name});
-            // }
-        });
+    } else if (domainMessage === 'clearCookies') {
+        chrome.runtime.sendMessage({ type: domainMessage, domain: document.location.host });
     }
 }
