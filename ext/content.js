e4922db8367f584348b8d0477925c67429114c16
Craig Walker <cwalker@overstock.com>
2018-09-04 21:23:59 -0600

    only sending them on certain URLs

--- ext/content.js
+++ ext/content.js
@@ -3,6 +3,7 @@ var domains = {
     "www.sltrib.com": 'tp-modal',
     "www.nytimes.com": 'clearCookies',
 };
+
 var domainMessage = domains[document.location.host];
 
 if (domainMessage) {
@@ -19,20 +20,28 @@ if (domainMessage) {
 
 function checkPopup() {
     if (domainMessage === 'tp-modal' && document.activeElement === document.body) {
-        var tpModal = document.getElementsByClassName('tp-modal')[0];
-        if (tpModal) {
-            clearInterval(interval);
-            var tpActive = document.getElementsByClassName('tp-backdrop tp-active')[0];
-            tpModal.parentNode.removeChild(tpModal);
-            tpActive.parentNode.removeChild(tpActive);
-            document.body.classList.remove('tp-modal-open');
-            window.scrollTo(0,0);
-        }
+        tpModal();
     } else if (domainMessage === 'clearCookies') {
-        clearInterval(interval);
-        var sendDomain = {
-            "www.nytimes.com": ".nytimes.com",
-        };
-        chrome.runtime.sendMessage({ type: domainMessage, domain: sendDomain[document.location.host] });
+        clearCookies();
     }
+}
+
+function tpModal() {
+    var tpModalEl = document.getElementsByClassName('tp-modal')[0];
+    if (tpModalEl) {
+        clearInterval(interval);
+        var tpActive = document.getElementsByClassName('tp-backdrop tp-active')[0];
+        tpModalEl.parentNode.removeChild(tpModalEl);
+        tpActive.parentNode.removeChild(tpActive);
+        document.body.classList.remove('tp-modal-open');
+        window.scrollTo(0,0);
+    }
+}
+
+function clearCookies() {
+    clearInterval(interval);
+    var sendDomain = {
+        "www.nytimes.com": ".nytimes.com",
+    };
+    chrome.runtime.sendMessage({ type: domainMessage, domain: sendDomain[document.location.host] });
 }
