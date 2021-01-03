8b48f231f800cd06d7820fdd7c2f095af8fe5f97
Craig Walker <craigwalker1123@gmail.com>
2021-01-03 14:41:16 -0800

    added Washington Post

--- ext/background.js
+++ ext/background.js
@@ -1,9 +1,25 @@
-chrome.runtime.onMessage.addListener(function(request) {
-    if (request.type === "clearCookies") {
-        chrome.cookies.getAll({ domain: request.domain }, function(cookies) {
-            for (var i = 0; i < cookies.length; i++) {
-                chrome.cookies.remove({ url: 'https://' + cookies[i].domain + cookies[i].path, name: cookies[i].name});
-            }
+chrome.runtime.onMessage.addListener(function (request) {
+  if (request.type === "clearCookies") {
+    chrome.cookies.getAll({ domain: request.domain }, function (cookies) {
+      for (var i = 0; i < cookies.length; i++) {
+        chrome.cookies.remove({
+          url: "https://" + cookies[i].domain + cookies[i].path,
+          name: cookies[i].name,
         });
-    }
+      }
+    });
+  }
 });
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function (details) {
+    return { cancel: true };
+  },
+  {
+    urls: [
+      "*://www.washingtonpost.com/subscribe/paywall/*",
+      "*://www.washingtonpost.com/pwapiv2/article/*",
+    ],
+  },
+  ["blocking"]
+);
--- ext/content.js
+++ ext/content.js
@@ -1,61 +1,69 @@
 var domains = {
-    "www.businessinsider.com": 'tp-modal',
-    "www.sltrib.com": 'tp-modal',
-    "www.nytimes.com": 'clearCookies',
-    "www.cnbc.com": 'pico',
+  "www.businessinsider.com": "tp-modal",
+  "www.sltrib.com": "tp-modal",
+  "www.nytimes.com": "clearCookies",
+  "www.cnbc.com": "pico",
 };
 
 var domainMessage = domains[document.location.host];
 
 if (domainMessage) {
-    var timesRun = 0;
-    var interval = setInterval(function(){
-        timesRun += 1;
-        if (timesRun === 10) {
-            clearInterval(interval);
-        } else {
-            checkPopup();
-        }
-    }, 2000);
+  var timesRun = 0;
+  var interval = setInterval(function () {
+    timesRun += 1;
+    if (timesRun === 8) {
+      clearInterval(interval);
+    } else {
+      checkPopup();
+    }
+  }, 2000);
 }
 
 function checkPopup() {
-    if (domainMessage === 'tp-modal' && document.activeElement === document.body) {
-        tpModal();
-    } else if (domainMessage === 'clearCookies') {
-        clearCookies();
-    } else if (domainMessage === 'pico') {
-        pico();
-    }
+  if (
+    domainMessage === "tp-modal" &&
+    document.activeElement === document.body
+  ) {
+    tpModal();
+  } else if (domainMessage === "clearCookies") {
+    clearCookies();
+  } else if (domainMessage === "pico") {
+    pico();
+  } else if (domainMessage === "paywall") {
+    paywall();
+  }
 }
 
 function tpModal() {
-    var tpModalEl = document.getElementsByClassName('tp-modal')[0];
-    if (tpModalEl) {
-        clearInterval(interval);
-        var tpActive = document.getElementsByClassName('tp-backdrop tp-active')[0];
-        tpModalEl.parentNode.removeChild(tpModalEl);
-        tpActive.parentNode.removeChild(tpActive);
-        document.body.classList.remove('tp-modal-open');
-        window.scrollTo(0,0);
-    }
+  var tpModalEl = document.getElementsByClassName("tp-modal")[0];
+  if (tpModalEl) {
+    clearInterval(interval);
+    var tpActive = document.getElementsByClassName("tp-backdrop tp-active")[0];
+    tpModalEl.parentNode.removeChild(tpModalEl);
+    tpActive.parentNode.removeChild(tpActive);
+    document.body.classList.remove("tp-modal-open");
+    window.scrollTo(0, 0);
+  }
 }
 
 function pico() {
-  var picoContent = document.getElementsByClassName('pico-content')[0];
+  var picoContent = document.getElementsByClassName("pico-content")[0];
   if (picoContent) {
-      clearInterval(interval);
-      var picoOverlay = document.getElementsByClassName('pico-overlay')[0];
-      picoOverlay.parentNode.removeChild(picoOverlay);
-      picoContent.parentNode.removeChild(picoContent);
-      window.scrollTo(0,0);
+    clearInterval(interval);
+    var picoOverlay = document.getElementsByClassName("pico-overlay")[0];
+    picoOverlay.parentNode.removeChild(picoOverlay);
+    picoContent.parentNode.removeChild(picoContent);
+    window.scrollTo(0, 0);
   }
 }
 
 function clearCookies() {
-    clearInterval(interval);
-    var sendDomain = {
-        "www.nytimes.com": ".nytimes.com",
-    };
-    chrome.runtime.sendMessage({ type: domainMessage, domain: sendDomain[document.location.host] });
+  clearInterval(interval);
+  var sendDomain = {
+    "www.nytimes.com": ".nytimes.com",
+  };
+  chrome.runtime.sendMessage({
+    type: domainMessage,
+    domain: sendDomain[document.location.host],
+  });
 }
